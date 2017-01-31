ff3e4a499e8131bb865bd0df36afa4feffd85f21
joan79 <joan79@gmail.com>
2017-01-31 08:26:07 -0500

    Test branch

--- amazono1.php
+++ amazono1.php
@@ -1,8 +1,9 @@
 <?php
 include "config.php";
-$message = "Hola, saludos de amazono";
+$message = "Hola, saludos de <b>amazono</b>";
 $bot_url    = "https://api.telegram.org/bot$botToken/";
 $url = $bot_url."sendMessage?chat_id=".$chat_id."&text=".urlencode($message);
 $urlGetMe = "https://api.telegram.org/bot$botToke/getMe";
 $a = file_get_contents($url);
 echo $a;
+echo "hola";
