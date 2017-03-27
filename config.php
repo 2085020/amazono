ade1ab76706e0dbf3c9f12d14dd6da1893067367
joan79 <joan79@gmail.com>
2017-03-27 06:14:22 -0400

    first files

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$botToken = "303282760:AAGg7ig2FZzLqNvMbATTOOi-U5yi_f1Fc98";
+$chat_id = "@amazono";
+define('AMAZON_API_KEY', 'AKIAJAEPGKAOOLKESBAA');
+define('AMAZON_SECRET_KEY', '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');
+define('AMAZON_ASSOCIATE_TAG', 'regconide-21');
