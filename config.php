d7fd14c7121e5cb74c2dccb812606c624b3e45de
Joan Cruz <joan79@gmail.com>
2017-03-27 12:17:52 +0200

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$botToken = "303282760:AAGg7ig2FZzLqNvMbATTOOi-U5yi_f1Fc98";
-$chat_id = "@amazono";
-define('AMAZON_API_KEY', 'TEST');
-define('AMAZON_SECRET_KEY', 'test');
-define('AMAZON_ASSOCIATE_TAG', 'test');
